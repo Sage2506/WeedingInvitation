abda5b578d284820b76f519805f64cfb2adb0839
German Eduardo Salazar Aranda <german@agricompra.com>
2023-01-10 17:01:33 -0700

    empty landing

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import castle from './assets/disney_castle.png';
+import './App.css';
+//<img src={castle} className="App-logo" alt="logo" />
+/*<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+></a>*/
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
